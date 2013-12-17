7039ce7c83bf7819eabd6021a3d8c6c8f010e460
iahs <iahs@users.noreply.github.com>
2013-12-17 17:46:56 -0500

    single port

--- frontend.js
+++ frontend.js
@@ -7,7 +7,7 @@ var express = require('express')
   , sys = require('sys')
   , jade = require('jade');
 
-// HTTP entrypoint
+    var clients = [];
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
@@ -18,31 +18,18 @@ app.get('/', function(req, res){
   res.render('remote.jade');
 });
 server.listen(8080);
+
 io.sockets.on('connection', function (socket) {
         socket.on('message', function (message) {
             var parts = message.toString('utf-8').split(':');
             if (parts[0] in clients) {
-            clients[parts[0]].write(parts[1]);
+            clients[parts[0]].emit('cmd',parts[1]);
         }
         });
+        socket.on('join', function(message){
+            var text = message.toString('utf-8');
+            if (text.substring(0,1) == '~') {
+                clients[text.substring(1)] = socket;
+            }
+        });
 });
-
-// Main server
-    net.createServer(onConnection).listen(8124);
-    var clients = [];
-    function onConnection(socket) {
-     socket.setNoDelay(true);
-      //sys.puts('client connected: ' + socket.remoteAddress);
-
-     socket.addListener("data", function (data) {
-        var text = data.toString('utf-8');
-      if (text.substring(0,1) == '~') {
-        clients[text.substring(1)] = socket;
-      }
-     });
-
-     socket.addListener("end", function () {
-      //sys.puts('end of connection');
-      this.end();
-     });
-    }
