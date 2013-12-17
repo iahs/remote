e16d8774281a51ab4d3efefb79cc223b0a5f3e42
iahs <iahs@users.noreply.github.com>
2013-12-17 16:58:47 -0500

    entrypoint

--- frontend.js
+++ frontend.js
@@ -4,8 +4,10 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server)
   , net = require('net')
-  , stream = net.createConnection(8124);
-var jade = require('jade');
+  , sys = require('sys')
+  , jade = require('jade');
+
+// HTTP entrypoint
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
@@ -18,6 +20,29 @@ app.get('/', function(req, res){
 server.listen(8080);
 io.sockets.on('connection', function (socket) {
         socket.on('message', function (message) {
-                stream.write(message);
+            var parts = message.toString('utf-8').split(':');
+            if (parts[0] in clients) {
+            clients[parts[0]].write(parts[1]);
+        }
         });
 });
+
+// Main server
+    net.createServer(onConnection).listen(8124);
+    var clients = [];
+    function onConnection(socket) {
+     socket.setNoDelay(true);
+      //sys.puts('client connected: ' + socket.remoteAddress);
+
+     socket.addListener("data", function (data) {
+        var text = data.toString('utf-8');
+      if (text.substring(0,1) == '~') {
+        clients[text.substring(1)] = socket;
+      }
+     });
+
+     socket.addListener("end", function () {
+      //sys.puts('end of connection');
+      this.end();
+     });
+    }
