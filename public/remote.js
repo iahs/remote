ea572b81b242aaa15720c68b8f348bd03b83010c
iahs <iahs@users.noreply.github.com>
2013-12-17 22:41:16 -0500

    persistent id

--- public/remote.js
+++ public/remote.js
@@ -10,4 +10,6 @@ $(function() {
 	$("#playpause").click(function() {sendMessage('playpause');});
 	$("#prev").click(function() {sendMessage('previous track');});
 	$("#next").click(function() {sendMessage('next track');});
+	$('#id').val($.cookie('id'));
+	$('#id').change(function() {$.cookie('id', $('#id').val(), { expires: 90 });});
 });
