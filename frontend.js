641bf728e44ca79764ab45c2e417f2bd17dd152a
iahs <iahs@users.noreply.github.com>
2013-12-19 13:21:36 -0500

    socket test

--- frontend.js
+++ frontend.js
@@ -20,8 +20,7 @@ app.get('/', function(req, res){
 });
 server.listen(process.env.PORT || 5000);
 io.configure(function () {
-  io.set("transports", ['WebSocket', 'xhr-polling']);
-  io.set('rememberTransport', false);
+  io.set("transports", ["xhr-polling"]);
   io.set("polling duration", 10);
 });
 io.sockets.on('connection', function (socket) {
